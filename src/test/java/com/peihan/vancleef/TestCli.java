084290a56c7060c1ac352061ba37440ff3d75217
zhaipeihan <zhaipeihan@outlook.com>
2018-05-14 18:57:26 +0800

    test merkle tree

--- src/test/java/com/peihan/vancleef/TestCli.java
+++ src/test/java/com/peihan/vancleef/TestCli.java
@@ -26,14 +26,14 @@ public class TestCli {
 
     @Test
     public void testBalanceCli() throws Exception {
-        String[] args = {"balance", "-address", peihan};
+        String[] args = {"balance", "-address", lushu};
         Cli cli = new Cli(args);
         cli.start();
     }
 
     @Test
     public void testSendCli() throws Exception {
-        String[] args = {"send", "-from", peihan, "-to", lushu, "-amount", "1"};
+        String[] args = {"send", "-from", peihan, "-to", lushu, "-amount", "5"};
         Cli cli = new Cli(args);
         cli.start();
     }
